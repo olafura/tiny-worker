decd09ac050a6783fe8406e77eb342064332d6d8
Olafur Arason <olafura@olafura.com>
2025-08-12 18:30:10 +0200

    feat: add auth to add shortener

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
+import { AuthKitProvider } from '@workos-inc/authkit-nextjs/components';
 import { Geist, Geist_Mono } from 'next/font/google';
+
 import './globals.css';
 
 const geistSans = Geist({
@@ -24,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AuthKitProvider>{children}</AuthKitProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
 import Create from '@/app/ui/create';
+import Link from 'next/link';
+import { getSignUpUrl, withAuth } from '@workos-inc/authkit-nextjs';
 
-export default function Page() {
+export default async function Page() {
+  // Retrieves the user from the session or returns `null` if no user is signed in
+  const { user } = await withAuth();
+
+  // Get the URL to redirect the user to AuthKit to sign up
+  const signUpUrl = await getSignUpUrl();
+
+  if (!user) {
+    return (
+      <>
+        <a href="/login">Sign in</a>
+        <Link href={signUpUrl}>Sign up</Link>
+      </>
+    );
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-(family-name:--font-geist-sans)">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
